42f62153afacb423ab524a8ebbef29fd25025659
Evgenij Zavgorodnij <razorval99@gmail.com>
2018-01-08 02:05:15 +0300

    Rename Семинар3.cpp to Семинар3(Б).cpp

---
+++ 1 semester/Семинар3(Б).cpp
@@ -0,0 +1,24 @@
+﻿#include"stdafx.h"
+#include<Windows.h>
+#include<cstdlib>
+#include<iostream>
+using namespace std;
+int main()
+{
+	const int n = 10;
+	float a[n];
+	for (int i = 0; i<n; i++)
+		cin >> a[i];
+	float min = a[0], nomer = 0, sum = 0;
+	for (int i = 0; i<n; i++)
+		if (a[i] < min)
+		{
+			min = a[i];
+			nomer = i;
+		}
+	for (int i = 0; i<nomer; i++)
+		sum += a[i];
+	cout << sum;
+	system("pause");
+	return 0;
+}
