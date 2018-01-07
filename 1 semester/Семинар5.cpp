6fa659b2bb0650d1b50d5b79d68174e3f06d9690
Evgenij Zavgorodnij <razorval99@gmail.com>
2018-01-08 02:11:52 +0300

    Rename Семинар3.cpp to Семинар5.cpp

---
+++ 1 semester/Семинар5.cpp
@@ -0,0 +1,32 @@
+﻿#include"stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+
+	ifstream fileStream("text.txt");
+	string str, res;
+	int i, max, j;
+	max = i = j = 0;
+	while (!fileStream.eof())
+	{
+		getline(fileStream, str);
+		j = 0;
+		for (i = 0; i<str.length(); ++i)
+		{
+			if (ispunct(str[i]))
+				++j;
+			if (j>max)
+			{
+				max = j;
+				res = str;
+			}
+		}
+	}
+	cout << res << endl;
+}
