cea44e1342c3d24cfa1e647177e0e76a724dd895
Evgenij Zavgorodnij <razorval99@gmail.com>
2018-01-08 02:00:44 +0300

    Rename Семинар3.cpp to Семинар3(А).cpp

---
+++ 1 semester/Семинар3(А).cpp
@@ -0,0 +1,28 @@
+﻿#include"stdafx.h"
+#include<Windows.h>
+#include<cstdlib>
+#include<iostream>
+using namespace std;
+int main()
+{
+	//A задание
+	setlocale(0,"");
+	const int n = 10;
+	float  Array[n];
+	cout << "Введите массив" << endl;
+		for (int i = 0; i <n; i++)
+		{
+			cin >> Array[i];
+		}
+		int proiz = 1;
+		for (int i = 0; i<n; i++)
+			if (Array[i]>0)
+			{
+				proiz *= Array[i];
+			}
+		cout << "Ответ" << proiz << endl;
+		system("pause");
+		return 0;
+
+}
+
